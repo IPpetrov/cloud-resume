5a8c541f0b559d9671ad251fb072c7917a08a7cf
ivo <ivo@Debian.myguest.virtualbox.org>
2024-01-28 17:13:30 +0200

    initial commit

---
+++ website/script.js
@@ -0,0 +1,8 @@
+const counter = document.getElementById('visitorCount');
+async function updateCounter() {
+    let response = await fetch("https://j0mmwoxgh6.execute-api.eu-central-1.amazonaws.com/prod/counter");
+    let data = await response.json();
+    counter.innerHTML = data
+}
+
+updateCounter();
