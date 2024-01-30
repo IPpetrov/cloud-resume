9b6e8003cf6798a937fd4de8d7b3bb8558c3ac78
ivo <ivo@Debian.myguest.virtualbox.org>
2024-01-30 20:38:11 +0200

    updating github actions for terraform

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,8 @@
 context("GET /counter", () => {
   it("gets the updated visitor counter, which should be higher than 0", () => {
     cy.request("GET", "https://j0mmwoxgh6.execute-api.eu-central-1.amazonaws.com/prod/counter").then((response) => {
-      if (response.status === 200 && response.body > 0) {
-        cy.get('#cypress-visit-count').should('be.visible')
-      } else {
-        cy.get('#cypress-visit-count-').should('not.be.visible');
-      }
+      expect(response.status).to.eq(200)
+      expect(response.body).to.be.greaterThan(0)
     })
   })
 })  
