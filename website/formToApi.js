3c0b991663b092cda8f110c363c8f22366afcc93
ivo <ivo@Debian.myguest.virtualbox.org>
2024-02-22 13:55:46 +0200

    fixing variables

--- website/formToApi.js
+++ website/formToApi.js
@@ -2,11 +2,11 @@ function formToApi(event) {
     event.preventDefault();
     var hiddenMessage = document.getElementById("hiddenMessage");
     var data = {
-        name: document.getElementsById('name').value,
-        email: document.getElementsById('email').value,
-        message: document.getElementsById('message').value
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
     }
-    fetch( "https://4qwmba7004.execute-api.eu-central-1.amazonaws.com/prod/sending" , {
+    fetch("https://0s7g5eahel.execute-api.eu-central-1.amazonaws.com/prod/send", {
         dataType: "json",
         crossDomain: "true",
         contentType: "application/json; charset=utf-8",
@@ -30,32 +30,3 @@ function clearForm(){
     email.value = "";
     message.value = "";
 }
-
-
-function invokeAWSAPI(e) {
-    e.preventDefault();           
-    var name = $("#form-name").val();
-    var phone = $("#form-phone").val();
-    var email = $("#form-email").val();
-    var msg = $("#form-message").val();
-    var data = {
-       name : name,
-       phone : phone,
-       email : email,
-       msg : msg
-     };$.ajax({
-      type: "POST",
-      url : "https://*************.us-east-1.amazonaws.com/dev/contact-us",
-      dataType: "json",
-      crossDomain: "true",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-      success: function () {
-        alert("Successful");
-        document.getElementById("contact-form").reset();
-    location.reload();
-      },
-      error: function () {
-        alert("unsuccessful");
-      }});
-  }
