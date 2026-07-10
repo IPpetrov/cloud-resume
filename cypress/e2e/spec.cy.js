94639748199b7ed48b4eb53141030894f021d316
Ivaylo Petrov <i.petrow@live.com>
2026-07-10 11:56:17 +0300

    change visitor counter url to the correct one

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,8 @@
-context("GET /counter", () => {
+context("GET /visitor-count-api", () => {
   it("gets the updated visitor counter, which should be higher than 0", () => {
-    cy.request("GET", "https://j0mmwoxgh6.execute-api.eu-central-1.amazonaws.com/prod/counter").then((response) => {
+    cy.request("GET", "https://j0mmwoxgh6.execute-api.eu-central-1.amazonaws.com/visitor-count-api").then((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).to.be.greaterThan(0)
     })
   })
-})  
+})
